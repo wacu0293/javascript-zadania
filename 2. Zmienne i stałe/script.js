3e3ae53f99303b0bf1c69fbbbe6dbb453039b498
WaclawWojcik <66508748+wacu0293@users.noreply.github.com>
2021-02-06 17:59:49 +0100

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,24 @@
+// Zadanie1
+
+let number = 2;
+let string = "dwa";
+let stringNumConcat = number + string;
+let logicValue = 3 < 2;
+let special = null;
+
+console.log(number, string, stringNumConcat, logicValue, special);
+
+// Zadanie2
+
+let num0 = 12;
+let num1 = 4;
+let sum = 0;
+sum = num0 + num1;
+console.log(sum);
+
+// Zadanie3
+
+let unknown;
+console.log(unknown);
+
+// Zwraca wartość undefined, ponieważ zmienna nie ma przypisanej wartości
