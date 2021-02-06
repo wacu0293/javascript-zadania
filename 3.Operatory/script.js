40cbdb6b2d03978ed00063ef7edb9714e804ca5c
WaclawWojcik <66508748+wacu0293@users.noreply.github.com>
2021-02-06 18:00:22 +0100

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,51 @@
+// Zadanie1
+let bool1 = true;
+let bool2 = false;
+
+console.log(bool1 === bool2);
+
+// Zadanie2
+
+let num1 = 12;
+let num2 = 5;
+let moduloResult = 0;
+moduloResult = num1 % num2;
+
+console.log(moduloResult);
+
+// Zadanie3
+
+let str1 = `Lorem`;
+let str2 = `Ipsum`;
+let stringsResult = "";
+stringsResult = str1 + str2;
+
+console.log(stringsResult);
+
+
+// Zadanie4
+
+var someNumber = 425;
+var someString = "425";
+
+console.log(someNumber == someString);
+console.log(someNumber === someString);
+
+// Operator == porównuje wartości// === wartości oraz typy.
+
+// Zadanie5
+let counter = 30;
+console.log(counter);
+counter++;
+console.log(counter);
+counter--;
+console.log(counter);
+
+
+// Zadanie6
+let num3 = 12;
+let num4 = 5;
+let result = 0;
+
+result = num3 > num4;
+console.log(result);
