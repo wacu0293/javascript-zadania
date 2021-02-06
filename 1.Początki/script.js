0e7958c6e3043f6c24c016c7d6bd88f892af19a1
WaclawWojcik <66508748+wacu0293@users.noreply.github.com>
2021-02-06 17:58:33 +0100

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,19 @@
+// Zadanie1
+
+console.log("Waclaw Wojcik");
+
+// Zadanie2
+
+console.log(12 +32);
+console.log(52 / 16);
+console.log(5 * 2);
+
+// Zadanie3
+
+console.log("Hello World");
+console.log(Hello World);
+console.log("Hello World);
+
+//  13 - wyświetla string "Hello World"
+//  14 - Syntax error - brak znaków "
+//  15 - Syntax error - brak
